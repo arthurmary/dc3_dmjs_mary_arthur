4cd203911f423ef74c73627edf369a65c7312d82
arthurmary <113183213+arthurmary@users.noreply.github.com>
2023-02-04 12:43:36 +0100

    ex02_finsh

---
+++ ex02/ft_print_reverse_string.js
@@ -0,0 +1,12 @@
+function reverse(str) {
+    let inverser = "";
+  
+    for (let i = string.length - 1; i >= 0; i--) {
+      inverser += string[i];
+    }
+  
+    return inverser;
+  }
+
+  console.log(reverse("Arthur"));
+
